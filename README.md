4e92d1b96a839f2774247ab006807842be736596
skavhar1998 <skavhar1998@gmail.com>
2023-04-13 14:19:19 +0530

    Added the maximum weight bipartite mathcing i.e, step 1 of the Algorithm

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Fairest_Rent_Division
 Based on paper of "Which is the Fairest (Rent Division) of them all"
+
+# Run
+python3 fair.py
---
+++ fair.py
@@ -0,0 +1,60 @@
+from pulp import *
+
+
+def get_input_nodes():
+    nodes = []
+    num_nodes = int(input("Enter number of students or rooms: "))
+    for i in range(num_nodes):
+        # nodes will start from 1
+        nodes.append(i+1)
+    return(nodes)
+    
+
+def get_input_weights():
+    wt = {}
+    for u in nodes:
+        wt[u] = {}
+        for v in nodes:
+            wt[u][v] = int(input("Enter the rent preferance by student "+ str(u)+ " for room "+ str(v) +" : "))
+    return(wt)
+
+    
+
+def solve_mwbm(nodes, wt):
+# A Wrapper func that uses pulp to formulate and solve a MWBM
+
+    prob = LpProblem("MWBM_Problem", LpMaximize)
+
+    #Create the decision variables
+    choices = LpVariable.dicts("edge", (nodes, nodes), 0, 1, LpInteger)
+
+    max_prob = []
+    for u in nodes:
+        for v in nodes:
+            max_prob.append(wt[u][v] * choices[u][v])
+
+    prob += lpSum(max_prob), "Total_weight"
+    
+    for u in nodes:
+        for v in nodes:
+            u_capacity_prob = []
+            v_capacity_prob = []
+            for w in nodes:
+                u_capacity_prob.append(choices[u][w])
+                v_capacity_prob.append(choices[w][u])
+            prob += lpSum(u_capacity_prob) <= 1, ""
+            prob += lpSum(v_capacity_prob) <= 1, ""
+
+    print(prob)
+    
+    prob.solve()
+
+    print("Status:", LpStatus[prob.status])
+    for v in prob.variables():
+        print(v.name, " = ", v.varValue )
+    print("Sum of wts of selected edges= ", value(prob.objective))
+
+
+nodes = get_input_nodes()
+wt= get_input_weights()
+solve_mwbm(nodes, wt)

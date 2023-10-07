e2eb5b007bb5774f070da410621ce9ed5ac93418
Santosh <skavhar1998@gmail.com>
2023-10-07 17:28:05 +0530

    maximinUtility solves the rent division problem for either the file or normal input

--- fair.py
+++ fair.py
@@ -1,5 +1,12 @@
 from pulp import *
+import sys
+import random
+import string
 
+
+room_list = []
+agent_list = []
+assignment = {}
 
 def get_input_nodes():
     nodes = []
@@ -15,7 +22,7 @@ def get_input_weights(nodes):
     for u in nodes:
         wt[u] = {}
         for v in nodes:
-            wt[u][v] = int(input("Enter the rent preference by student "+ str(u)+ " for room "+ str(v) +" : "))
+            wt[u][v] = int(input("Enter the preffered rent by student "+ str(u)+ " for room "+ str(v) +" : "))
     return(wt)
 
     
@@ -55,7 +62,145 @@ def solve_mwbm(nodes, wt):
         print(v.name, " = ", v.varValue )
     print("Sum of wts of selected edges= ", value(prob.objective))
 
+def createFile(nodes, wt):
+    # 16 random letters input file to be created
+    res = "Inputs/input_" + ''.join(random.choices(string.ascii_lowercase, k=16)) + ".txt"
+    f = open(res, "w")
+    # We can get number of nodes as the first line
+    f.write(str(len(nodes))+"\n")
+    rent = 0
+    print(wt)
+    # Second line will be the sum of rent of all rooms for any agent, consider first agent
+    for i in nodes:
+        rent += wt[1][i]
+    f.write(str(rent) + "\n")
+    for i in nodes:
+        for j in nodes:
+            f.write(str(i) + " " + str(j) + " " + str(wt[i][j])+"\n")
+    f.close()
+    return res
 
-nodes = get_input_nodes()
-wt= get_input_weights(nodes)
-solve_mwbm(nodes, wt)
+def maximinUtility(file_name):
+    with open(file_name, 'r') as file:
+        n = int(file.readline())
+        rent = int(file.readline())
+        values = {}
+        agent_set = set()
+        room_set = set()
+
+        for _ in range(n * n):
+            tokens = file.readline().split()
+            agent_id = tokens[0]
+            agent_set.add(agent_id)
+            agent_list.append(agent_id)
+            room_id = tokens[1]
+            room_set.add(room_id)
+            room_list.append(room_id)
+            value = int(float(tokens[2]))
+            if agent_id not in values:
+                values[agent_id] = {}
+            values[agent_id][room_id] = value
+
+    # First, find a welfare-maximizing allocation
+    allocation = welfareMaximize(values, agent_set, room_set)
+
+    # Now, find nonnegative price vector if possible
+    prices = maximinPrices(values, agent_set, room_set, allocation, rent, True)
+    if prices is None:
+        #prices = maximinPrices(values, agent_set, room_set, allocation, rent, False)
+        #if prices is None:
+        print("failure")
+        return
+
+    for agent in agent_set:
+        room = allocation[agent]
+        price = prices[room]
+        print(f"{agent} {room} {price}")
+
+
+def welfareMaximize(values, agent_set, room_set):
+    prob = LpProblem("WelfareMaximization", LpMaximize)
+    variables = {}
+    for a in agent_set:
+        variables[a] = {}
+        for r in room_set:
+            variables[a][r] = LpVariable(f"x_{a}_{r}", 0, 1, LpBinary)
+
+    # Objective is total welfare
+    prob += lpSum(values[a][r] * variables[a][r] for a in agent_set for r in room_set)
+
+    # Each agent assigned 1 room
+    for a in agent_set:
+        prob += lpSum(variables[a][r] for r in room_set) == 1
+
+    # Each room assigned 1 agent
+    for r in room_set:
+        prob += lpSum(variables[a][r] for a in agent_set) == 1
+
+
+    print(prob)
+    prob.solve()
+
+    # Get the assignment
+    assignment = {}
+    for a in agent_set:
+        for r in room_set:
+            if variables[a][r].value() == 1:
+                assignment[a] = r
+    return assignment
+
+
+def maximinPrices(values, agent_set, room_set, assignment, rent, nonnegative_prices):
+    prob = LpProblem("MaximinPrices", LpMinimize)
+    price_variables = {}
+    for r in room_set:
+        if nonnegative_prices:
+            price_variables[r] = LpVariable(f"p_{r}", 0, rent)
+        else:
+            price_variables[r] = LpVariable(f"p_{r}", 0)
+
+    # Objective is maximize minimum utility (or minimize negative of minimum utility)
+    min_utility = LpVariable("y", 0)
+    prob += min_utility
+
+    # Ensure prices sum to rent
+    prob += lpSum(price_variables[r] for r in room_set) == rent
+
+    #print(agent_set, values, assignment)
+    # Ensure envy-free
+    for i in agent_list:
+        for j in room_list:
+            if i == j:
+                continue
+            prob += price_variables[j] - price_variables[assignment[i]] >= values[i][j] - values[i][assignment[i]]
+
+    # Bound minimum utility
+    for i in agent_set:
+        prob += min_utility >= values[i][assignment[i]] - price_variables[assignment[i]]
+
+    try:
+        print(prob)
+        prob.solve()
+    except:
+        print("Error occured!")
+        return
+    
+    if LpStatus[prob.status] == 'Optimal':
+        prices = {}
+        for i in agent_set:
+            room = assignment[i]
+            prices[room] = price_variables[room].value()
+        return prices
+    return None
+
+
+if __name__ == "__main__":
+    # Default file
+    file_name="Other/input3.txt"
+    if len( sys.argv ) > 1:
+        file_name = sys.argv[1]
+    else:
+        nodes = get_input_nodes()
+        wt= get_input_weights(nodes)
+        file_name = createFile(nodes, wt)
+    maximinUtility(file_name)

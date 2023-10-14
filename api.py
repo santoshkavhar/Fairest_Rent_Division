c5aa07c6d9ba2999ba56bbaee60210e962c545da
skavhar1998 <skavhar1998@gmail.com>
2023-10-14 15:31:11 +0530

    generate_csv used instead of normal text file processor.

--- api.py
+++ api.py
@@ -5,9 +5,6 @@ from helper import *
 from fair import *
 from inputs import *
 
-room_list = []
-agent_list = []
-assignment = {}
 # Default file
 file_name="Other/input3.txt"
 
@@ -23,18 +20,19 @@ def calculate_rent():
     num_renters = data.get('renters', 0)
     num_rooms = data.get('rooms', 0)
     rent_data = data.get('rentData', [[]])
-    print(num_rooms, num_renters, rent_data)
+    # print(num_rooms, num_renters, rent_data)
     renters_list = list(range(1, num_renters+1))
     rooms_list = list(range(1, num_rooms+1))
-    wt = convert_rent_data(rent_data)
-    file_name = create_file(renters_list, wt)
+    # Rent data should be in matrix format
+    file_path = generate_csv(rent_data)
     
-    ret_value = maximin_utility(file_name)
+    ret_value = maximin_utility(file_path)
     if ret_value is None:
-        return jsonify({'file_name': file_name, 'error':'Validation failed!'})
+        return jsonify({'file_path': file_path, 'error':'Validation failed!'})
     rooms_list, renters_list, rents = ret_value
+
     response = {
-        'file_name': file_name,
+        'file_path': file_path,
         'renters': renters_list,
         'rooms': rooms_list,
         'rents': rents

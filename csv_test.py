abdd136f3293dd8c733c1b566a5edcd632a3b48e
skavhar1998 <skavhar1998@gmail.com>
2023-10-15 14:51:13 +0530

    csv is at start of name in create and test file. test_csv_files added to test complete folder.

---
+++ csv_test.py
@@ -0,0 +1,16 @@
+import os
+from helper import *
+from fair import *
+
+def test_csv_files(folder_name):
+    # Check if the folder exists
+    if folder_name is None or not os.path.exists(folder_name):
+        failure(f'Please provide correct folder name. Given Folder does not exist.')
+        return
+
+    # List all files in the folder
+    files = os.listdir(folder_name)
+    for file in files:
+        file_path  = os.path.join(folder_name, file)
+        help(f'Testing file: '+ file_path)
+        maximin_utility(file_path)

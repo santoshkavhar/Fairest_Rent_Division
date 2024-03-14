cb4f62d4405a71aa1c9e9bbc34a531f957feedbb
skavhar1998 <skavhar1998@gmail.com>
2024-03-14 17:31:54 +0530

    Hostel API added

--- api.py
+++ api.py
@@ -47,5 +47,46 @@ def calculate_rent():
     return jsonify(response)
 
 
+@app.route("/hostel_api", methods=["GET", "POST"])
+@cross_origin()
+def calculate_hostel_rent():
+
+    data = request.get_json()
+
+    num_renters = data.get("renters", 0)
+    num_floors = data.get("floors", 0)
+    # TODO: Add capacity
+    # capacity = data.get("capacity",0)
+    rent_data = data.get("rentData", [[]])
+
+    # print(num_floors, num_renters, rent_data)
+    renters_list = list(range(1, num_renters + 1))
+    rooms_list = list(range(1, num_floors + 1))
+
+    print(renters_list,rooms_list)
+    # Rent data is already in matrix format
+    file_path = generate_csv(rent_data)
+
+    ret_value = maximin_utility(file_path)
+
+    if ret_value is None:
+        err_response = {
+            "file_path": file_path,
+            "error": "Validation failed or No solution found!"
+        }
+        return jsonify(err_response)
+
+    rooms_list, renters_list, rents = ret_value
+
+    response = {
+        "file_path": file_path,
+        "renters": renters_list,
+        "floors": rooms_list,
+        "rents": rents,
+    }
+
+    return jsonify(response)
+
+
 def run():
     app.run(host="127.0.0.1", port=5000)
